debf8301368cee99b17d954f65186ff7448a96e0
ejelome <ejelome@gmail.com>
2020-10-17 21:19:55 +0200

    Add useReducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useReducer } from "react";
 
-const App = () => <h1>hello, world</h1>;
+const ACTIONS = {
+  INCREMENT: "increment",
+};
+
+const reducer = (state, { type }) => {
+  const { INCREMENT } = ACTIONS;
+  let { count } = state;
+
+  switch (type) {
+    case INCREMENT:
+      count += 1;
+      return { ...state, count };
+    default:
+      return state;
+  }
+};
+
+const initialState = { count: 0 };
+const init = (initialState) => initialState;
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState, init);
+  const { count } = state;
+
+  const { INCREMENT } = ACTIONS;
+  const handleIncrement = () => dispatch({ type: INCREMENT });
+
+  return (
+    <>
+      <code>{count}</code>
+      <button onClick={handleIncrement}>+</button>
+    </>
+  );
+};
 
 export default App;
